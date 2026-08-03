b0f566da07a0af254d7b26ffeda58da7200bbbb9
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-08-03 19:18:22 +0300

    add `injections`

---
+++ queries/rl/injections.scm
@@ -0,0 +1,13 @@
+((doc_comment) @injection.content
+  (#set! injection.language "markdown")
+  (#offset! @injection.content 0 3 0 0))
+
+(call_expression
+  function: [
+    (identifier) @_fn
+    (path_expression) @_fn
+  ]
+  arguments: (string_literal) @injection.content
+  (#any-of? @_fn "compile" "std::c::compile")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "c"))
